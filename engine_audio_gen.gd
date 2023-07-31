7d2cfbe4d44b942ad4b80202fa04c0c565574595
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2023-07-31 10:49:51 +0300

    Refactor audio generation

--- engine_audio_gen.gd
+++ engine_audio_gen.gd
@@ -19,22 +19,44 @@ func _ready():
 
 
 func _process(delta: float) -> void:
-	var firing_hz = engine_params.cylinder_count * rpm / 120
 	var engine_phase = rpm / 120
+	var cylinder_phase = engine_params.cylinder_count * engine_phase
 	
-#	print_debug(firing_hz)
-#	print_debug(engine_phase)
-	
-	fill_buffer(firing_hz)
+	fill_buffer(cylinder_phase)
 
 
 func fill_buffer(pulse_freq: float):
 	var increment = pulse_freq / sample_hz
 	var frames_available = playback.get_frames_available()
+	
+	var exhaust := 0.0
+	var intake := 0.0
+	var piston := 0.0
+	var ignition := 0.0
 
 	for i in range(frames_available):
-		var frame := sin(phase * TAU) * sin(phase * 1.5 * TAU)
+		exhaust = exhaust_valve(phase)
+		intake = intake_valve(phase)
+		piston = piston_motion(phase)
+		ignition = ignition_fire(phase, 0.1)
+		
+		var frame := exhaust + intake + piston + ignition
+		
 		playback.push_frame(Vector2.ONE * frame)
 		phase = fmod(phase + increment, 1.0)
 
-	
+
+func exhaust_valve(x: float) -> float:
+	return -sin(4.0 * PI * x) if x > 0.75 and x < 1.0 else 0.0
+
+
+func intake_valve(x: float) -> float:
+	return sin(4.0 * PI * x) if x > 0.0 and x < 0.25 else 0.0
+
+
+func piston_motion(x: float) -> float:
+	return cos(4.0 * PI * x)
+
+
+func ignition_fire(x: float, t: float) -> float:
+	return sin(2.0 * PI * (x * t + 0.5)) if x > 0.0 and x < t else 0.0
